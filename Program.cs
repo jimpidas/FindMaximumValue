f99d0f48b8fc1359845f687db490fdf60530370b
unknown <jimpidas>
2021-05-24 23:06:33 +0530

    [Jyosmita]Added:Given maximum number at second position should return the same

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace FindMaximum
     {
         static void Main(string[] args)
         {
-          int max= FindMaximumFromThree.MaximumIntegerNumber(4, 4, 4);
+          int max= FindMaximumFromThree.MaximumIntegerNumber(4, 7, 6);
             Console.WriteLine("The maximum value is :" + max);
         }
     }
