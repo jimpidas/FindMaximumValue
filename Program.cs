4f12ba3f3a3d0fd7bb6e0a240a9d963ecefe23a3
unknown <jimpidas>
2021-05-24 23:01:26 +0530

    [Jyosmita]Added:Welcome to Find maximum value program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FindMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int max= FindMaximumFromThree.MaximumIntegerNumber(3, 3, 3);
+            Console.WriteLine("The maximum value is :" + max);
+        }
+    }
+}
