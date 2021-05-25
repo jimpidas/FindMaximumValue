f68c17597ca208ecd10741c011d9cea2e13801ce
unknown <jimpidas>
2021-05-25 16:59:10 +0530

    [Jyosmita]Added:extend the program to take more then 3 parameter as input

--- FindMaximumFromThree.cs
+++ FindMaximumFromThree.cs
@@ -6,14 +6,15 @@ namespace FindMaximum
 {
     class FindMaximumFromThree
     {
-        public static int  MaximumIntegerNumber(int firstValue,int SecondValue,int ThirdValue)
+
+        public static void  MaximumNumber(int firstValue, int SecondValue,int ThirdValue)
         {
             if (firstValue.CompareTo(SecondValue) > 0 && firstValue.CompareTo(ThirdValue) > 0)
-                return firstValue;
+                Console.WriteLine( firstValue);
             if (SecondValue.CompareTo(firstValue) > 0 && SecondValue.CompareTo(ThirdValue) > 0)
-                return SecondValue;
+                Console.WriteLine(SecondValue);
             if (ThirdValue.CompareTo(firstValue) > 0 && ThirdValue.CompareTo(SecondValue) > 0)
-                return ThirdValue;
+                Console.WriteLine(ThirdValue);
             else
                 throw new Exception("All numbers are same");
         }
